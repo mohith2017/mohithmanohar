7ef5ef32c6df04d396aac70a08f620eb8054de3c
mohith2017 <mohith.m.venkat@gmail.com>
2024-03-15 03:08:01 -0400

    Initial website commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mohith Manohar's Portfolio
+
+## About me
+My portfolio as an AI/ML and Full stack developer with 3+ years of experience and a MS in Computer Science from Columbia University.
+
+## Other important links:
+- [Linkedin](https://linkedin.com/in/mohithmanoharcu)
+- [GitHub](https://github.com/mohith2017)
+
+
+## Tech Stack
+
+- Latest Next.js 13 features
+- Next.js App Router
+- Next.js Server Actions
+- Client & Server Components
+- TypeScript (Beginner & Intermediate)
+- Tailwind CSS
+- Context API
+- Advanced Animations with Framer Motion
+- React.Email & Resend
+- Custom React hooks
+- Fresh, modern UI design
+- Light & Dark mode
+- Responsive website
+
+
---
+++ components/intro.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { BlinkedBox } from "./ui/blinkedbox";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+export default function Intro() {
+  const { ref } = useSectionInView("Mohith", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const today = new Date();
+
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://i.postimg.cc/655kg0fy/IMG-7471.jpg"
+              alt="Mohith portrait"
+              width="192"
+              height="192"
+              quality="100"
+              priority={true}
+              className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-2 text-xl font-medium !leading-[1.5] sm:text-3xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Hi, I'm 
+        <span className="font-bold"> Mohith.</span> I am a{" "}
+        Software Engineer with a unique blend of{" "}
+        <span className="font-bold">AI/ML, Gen AI and Full Stack</span> experience{" "}
+        <span className="font-bold">for 3+ years</span>. I enjoy
+        building <span className="italic">products(websites, apps, models, pipelines) from start to finish</span>. 
+        
+        <span className="underline"></span>
+        
+      </motion.h1>
+      
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-6 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/Mohith_Manohar.pdf"
+          download
+        >
+          Resume{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com/in/mohithmanoharcu"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/mohith2017"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+
+        <div className="flex justify-end">
+        <HoverCard>
+          <HoverCardTrigger asChild>
+        <BlinkedBox />
+        </HoverCardTrigger>
+        <HoverCardContent className="w-80">
+             <p className="text-sm">
+             <h4 className="text-sm font-semibold">Actively Interviewing at mid stages!!</h4>
+              Updated: 15 Mar '24
+            </p>      
+        </HoverCardContent>
+        </HoverCard>
+
+        </div>
+      </motion.div>
+    </section>
+  );
+}
